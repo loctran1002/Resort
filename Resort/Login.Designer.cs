ad4afb2bdcb9184b87652260717c1872c065d15f
loctran1002 <tienlocak14@gmail.com>
2021-11-30 00:19:03 +0700

    Finish Login feature

--- Resort/Login.Designer.cs
+++ Resort/Login.Designer.cs
@@ -33,8 +33,9 @@ namespace Resort
             this.button1 = new System.Windows.Forms.Button();
             this.User = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -43,12 +44,12 @@ namespace Resort
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
-            this.button1.Location = new System.Drawing.Point(512, 397);
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(639, 369);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 54);
+            this.button1.Size = new System.Drawing.Size(175, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -58,12 +59,13 @@ namespace Resort
             // 
             this.User.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.User.AutoSize = true;
-            this.User.BackColor = System.Drawing.Color.White;
-            this.User.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.User.Location = new System.Drawing.Point(357, 234);
+            this.User.BackColor = System.Drawing.Color.Transparent;
+            this.User.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User.ForeColor = System.Drawing.Color.Aqua;
+            this.User.Location = new System.Drawing.Point(261, 163);
             this.User.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.User.Name = "User";
-            this.User.Size = new System.Drawing.Size(124, 29);
+            this.User.Size = new System.Drawing.Size(122, 37);
             this.User.TabIndex = 1;
             this.User.Text = "Username";
             this.User.Click += new System.EventHandler(this.label1_Click);
@@ -72,71 +74,95 @@ namespace Resort
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(357, 331);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(261, 267);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.Size = new System.Drawing.Size(117, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // textUsername
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(512, 224);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.textBox1.Size = new System.Drawing.Size(395, 45);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textUsername.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textUsername.BackColor = System.Drawing.Color.YellowGreen;
+            this.textUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUsername.ForeColor = System.Drawing.Color.Purple;
+            this.textUsername.Location = new System.Drawing.Point(419, 163);
+            this.textUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textUsername.Size = new System.Drawing.Size(395, 37);
+            this.textUsername.TabIndex = 3;
+            this.textUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // textPassword
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(512, 321);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '@';
-            this.textBox2.Size = new System.Drawing.Size(395, 45);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textPassword.BackColor = System.Drawing.Color.YellowGreen;
+            this.textPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textPassword.ForeColor = System.Drawing.Color.Purple;
+            this.textPassword.Location = new System.Drawing.Point(419, 267);
+            this.textPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textPassword.Size = new System.Drawing.Size(395, 38);
+            this.textPassword.TabIndex = 4;
+            this.textPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(498, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 85);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "LOGIN";
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(732, 397);
+            this.button2.Location = new System.Drawing.Point(419, 369);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(175, 54);
+            this.button2.Size = new System.Drawing.Size(175, 58);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Register";
+            this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1218, 631);
+            this.ClientSize = new System.Drawing.Size(1076, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUsername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.User);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -152,8 +178,9 @@ namespace Resort
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label User;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
     }
 }
--- Resort/Login.cs
+++ Resort/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Resort
 {
@@ -16,6 +17,8 @@ namespace Resort
         {
             InitializeComponent();
         }
+
+        SqlConnection link = new SqlConnection(@"Data Source=DESKTOP-D59BBBC;Initial Catalog=RESORT;Persist Security Info=True;User ID=sManager;Password=Resort123");
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,7 +32,7 @@ namespace Resort
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            BackColor = Color.FromArgb(100, 0, 0, 0);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -39,12 +42,39 @@ namespace Resort
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            try
+            {
+                link.Open();
+                //string query = "SELECT * FROM KhachHang WHERE Username = '" + textUsername.Text.Trim() + "' AND Password = '" + textPassword.Text.Trim() + "'";
+                //SqlDataAdapter cmd = new SqlDataAdapter(query, link);
+                //DataTable table = new DataTable();
+                //cmd.Fill(table);
+                //if (table.Rows.Count == 1)
+                if (textUsername.Text.Trim() == "sManager" && textPassword.Text.Trim() == "Resort123")
+                {
+                    //Move to main
+                    this.Hide();
+                    //main.Show();
+                    MessageBox.Show("Login successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Check username and password again!!");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("System Error");
+            }
+            finally
+            {
+                link.Close();
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-
+            BackColor = Color.FromArgb(100, 0, 0, 0);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -54,8 +84,7 @@ namespace Resort
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
-
     }
 }
